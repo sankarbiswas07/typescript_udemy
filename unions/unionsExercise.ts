7abec02d79532312ce3600bcff637425efa3f467
Sankar Prasad Biswas <sankarbiswas07@gmail.com>
2023-07-02 01:40:00 +0530

    section 7: Union

---
+++ unions/unionsExercise.ts
@@ -0,0 +1,79 @@
+// **********************************************
+// ******************* PART 1 *******************
+// **********************************************
+// Create a variable called highScore that can be a number OR a boolean
+let highScore: (number | boolean) = 10
+highScore = true
+
+// **********************************************
+// ******************* PART 2 *******************
+// **********************************************
+// create an array called stuff
+// it can be an array of numbers OR an array of strings
+// it cannot be an array of numbers and strings (mixed together)
+let stuff: number[] | string[] = ["a", "1"]
+// **********************************************
+// ******************* PART 3 *******************
+// **********************************************
+// Create a literal type called SkillLevel
+// There are 4 allowed values: "Beginner", "Intermediate", "Advanced", and "Expert"
+type SkillLevel = "Beginner"
+  | "Intermediate"
+  | "Advanced"
+  | "Expert"
+// **********************************************
+// ******************* PART 4 *******************
+// **********************************************
+// Create a type called SkiSchoolStudent
+// name must be a string
+// age must be a number
+// sport must be "ski" or "snowboard"
+// level must be a value from the SkillLevel type (from above)
+
+type SkiSchoolStudent = {
+  name: string,
+  age: number,
+  sport: "ski" | "snowboard",
+  level: SkillLevel
+}
+
+// **********************************************
+// ******************* PART 5 *******************
+// **********************************************
+// Define a type to represent an RGB color
+// r should be a number
+// g should be a number
+// b should be a number
+
+type RGB = {
+  r: number,
+  g: number,
+  b: number,
+}
+
+// Define a type to represent an HSL color
+// h should be a number
+// s should be a number
+// l should be a number
+
+type HSL = {
+  h: number,
+  s: number,
+  l: number,
+}
+
+// Create an array called colors that can hold a mixture of RGB and HSL color types
+const colors: (HSL | RGB)[] = [{ r: 1, g: 2, b: 3 }, { h: 4, s: 4, l: 5 }]
+// **********************************************
+// ******************* PART 6 *******************
+// **********************************************
+// Write a function called greet that accepts a single string OR an array of strings
+// It should print "Hello, <name>" for that single person OR greet each person in the array with the same format
+
+function greet(names: string | string[]): void {
+  if (Array.isArray(names)) {
+    names.forEach(name => console.log(`Hello, ${names}`))
+  } else {
+    console.log(`Hello, ${names}`)
+  }
+}
