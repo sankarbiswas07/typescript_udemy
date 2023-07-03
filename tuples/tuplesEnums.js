e2a0d4558f95f3459cdb1e2f9fb85cbb0c42aeca
Sankar Prasad Biswas <sankarbiswas07@gmail.com>
2023-07-03 21:50:31 +0530

    section 8: Tuples & Enums

---
+++ tuples/tuplesEnums.js
@@ -0,0 +1,38 @@
+// These are NOT tuples:
+// const stuff: (string | number)[] = [1,'asd', 'asdasd', 'asdasd', 2]
+// const color: number[] = [23,45,234,234]
+// This is a tuple!
+var color = [255, 0, 45];
+var goodRes = [200, "OK"];
+// An array of tuples:
+var responses = [
+    [404, "Not Found"],
+    [200, "OK"],
+];
+// Enum Example:
+var OrderStatus;
+(function (OrderStatus) {
+    OrderStatus[OrderStatus["PENDING"] = 0] = "PENDING";
+    OrderStatus[OrderStatus["SHIPPED"] = 1] = "SHIPPED";
+    OrderStatus[OrderStatus["DELIVERED"] = 2] = "DELIVERED";
+    OrderStatus[OrderStatus["RETURNED"] = 3] = "RETURNED";
+})(OrderStatus || (OrderStatus = {}));
+var myStatus = OrderStatus.DELIVERED;
+function isDelivered(status) {
+    return status === OrderStatus.DELIVERED;
+}
+isDelivered(OrderStatus.RETURNED);
+// String Enum:
+var ArrowKeys;
+(function (ArrowKeys) {
+    ArrowKeys["UP"] = "up";
+    ArrowKeys["DOWN"] = "down";
+    ArrowKeys["LEFT"] = "left";
+    ArrowKeys["RIGHT"] = "right";
+})(ArrowKeys || (ArrowKeys = {}));
+var arrKey = {
+    key: ArrowKeys.UP
+};
+var arrNewKey = {
+    key: "down" /* ArrowNewKeys.DOWN */
+};
