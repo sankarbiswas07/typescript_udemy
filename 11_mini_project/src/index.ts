9d3fa046fd3ab035656d2da0801e102895b75c22
Sankar Prasad Biswas <sankarbiswas07@gmail.com>
2023-07-05 04:43:20 +0530

    section 11.72 >  Building todo list

--- 11_mini_project/src/index.ts
+++ 11_mini_project/src/index.ts
@@ -1,9 +1,47 @@
-function printDouble(msg: string) {
-  console.log(msg)
-  console.log(msg)
+
+//  =>  Running Note:
+//  =>  ! means, it will always be in the html (best practice to know that it will/should be on the page load).
+//       otherwise optional chaining (?.) is OK.
+//  =>  as HTMLInputElement : it is type casting as typeScript is not know input.value type
+
+
+const btn = document.getElementById("btn") as HTMLButtonElement
+const todoInput = document.getElementById("todoId")! as HTMLInputElement
+const form = document.querySelector("form")!
+const list = document.getElementById("todoList")!
+
+function _todoValidator(msg: string) {
+  if (!msg || msg.trim() == '') {
+    throw new Error("Todo is empty")
+  }
+  return msg.trim()
 }
 
+function _appendInList(msg: string) {
+  const span = document.createElement("span");
+  const label = document.createElement("label");
+  const checkbox = document.createElement("input");
+  const lineBreak = document.createElement("br");
 
-document.getElementById("btn")?.addEventListener("click", () => {
-  printDouble("Hello World!!! ")
-})
+  checkbox.type = "checkbox";
+  checkbox.id = msg; // Specify the 'id' attribute value to match the 'for' attribute of the label
+  span.appendChild(checkbox); // Append the checkbox element to the span element
+
+  label.textContent = msg;
+  label.htmlFor = msg; // Specify the 'for' attribute value
+
+  span.appendChild(label); // Append the label element to the span element
+  span.appendChild(lineBreak); // Append the line break element to the span element
+  list.append(span)
+}
+
+function _printMessageHandler(e: SubmitEvent) {
+  e.preventDefault() // to prevent form's default page load behavior
+
+  _todoValidator(todoInput.value)
+  _appendInList(todoInput.value)
+
+  todoInput.value = ""
+}
+
+form?.addEventListener("submit", _printMessageHandler)
