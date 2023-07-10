b11b9abb69ed4f91cfe75fb97533df695ddd22bf
Sankar Prasad Biswas <sankarbiswas07@gmail.com>
2023-07-11 01:07:13 +0530

    factory - I - example

--- js_class_recap/src/index.ts
+++ js_class_recap/src/index.ts
@@ -1,55 +1,50 @@
+// Abstract Class
+abstract class Animal {
+  abstract makeSound(): void
+}
 
-/**
- * Singleton design pattern.
- * SOme use-case:
- * - Resource sharing
- * - Centralized configuration
- * - Global state management
- * - Caching
- * - Dependency injection
- * - Easy access to functionality
- */
-class Logger {
-  // static method is belong to class, not available in object
-  // private access specifier used to make the variable private to Logger class
-  private static instance: Logger
-
-  private constructor(
-    private reset: string = "\x1b[0m",
-    private red: string = "\x1b[31m",
-    private green: string = "\x1b[32m",
-    private yellow: string = "\x1b[33m",
-    private blue: string = "\x1b[34m"
-  ) {
-    // Private constructor to prevent instantiate
-    // New is not allowed
-  }
-
-  // Log() is public and a static method. available in Logger.log()
-  public static log(): Logger {
-    if (!Logger.instance) {
-      Logger.instance = new Logger()
-    }
-    return Logger.instance
-  }
-
-  public info(message: string): void {
-    console.log(`${this.blue}${message}${this.reset}`)
-  }
-  public warn(message: string): void {
-    console.log(`${this.yellow}${message}${this.reset}`)
-  }
-  public success(message: string): void {
-    console.log(`${this.green}${message}${this.reset}`)
-  }
-  public error(message: string): void {
-    console.log(`${this.red}${message}${this.reset}`)
+// concrete class should implement makeSound
+class Dog extends Animal {
+  public makeSound(): void {
+    console.log("WOFF!! WOFF!!")
   }
 }
 
-const log = Logger.log()
 
-log.info("Info: I am a singleton class")
-log.success("I am green")
-log.warn("I am yellow")
-log.error("I am red")
+class Cat extends Animal {
+  public makeSound(): void {
+    console.log("ME0W!! ME0W!!")
+  }
+}
+
+// Abstract Creator: AnimalCreator is the abstract base class for all creators. 
+// It declares an abstract factory method createAnimal() that returns an Animal object.
+abstract class AnimalCreator {
+  abstract createAnimal(): Animal
+
+  // Common business logic for all creators
+  public introduceAnimal(): void {
+    const animal = this.createAnimal()
+    animal.makeSound()
+  }
+}
+
+// Concrete Creators
+class DogCreator extends AnimalCreator {
+  createAnimal(): Animal {
+    return new Dog();
+  }
+}
+
+class CatCreator extends AnimalCreator {
+  createAnimal(): Animal {
+    return new Cat();
+  }
+}
+
+// Usage
+const dogCreator: AnimalCreator = new DogCreator();
+dogCreator.introduceAnimal();
+
+const catCreator: AnimalCreator = new CatCreator();
+catCreator.introduceAnimal();
