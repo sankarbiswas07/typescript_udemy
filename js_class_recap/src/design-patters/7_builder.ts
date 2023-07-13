f0aa89737948b013c46a4b4e189859f2dceda191
Sankar Prasad Biswas <sankarbiswas07@gmail.com>
2023-07-13 23:56:24 +0530

    pattern - 7 - builder - a few notes (rough plan)

--- js_class_recap/src/design-patters/7_builder.ts
+++ js_class_recap/src/design-patters/7_builder.ts
@@ -6,3 +6,43 @@
  * and the order can include multiple pizzas with different specifications.
  * 
  */
+
+/***
+ * 11:20 PM
+ * Solution:
+ * Identifying domains : Two domain here, Pizza & the Order. 
+ * Pizza has it's own types -
+ *      type toppings = string[]
+ *      type crust = "thin" | "cheese" | "thick" | "stuffed"
+ *      type size = "small" | "medium" | "large" | "extra large"
+ * User can customize their `Order`
+ * 
+ * Solution by builder pattern -
+ * Builder pattern has 4 pillars - 
+ *  Product  - The product is the thing which need to build,
+ *  Builder - Builder interface the method signatures plus a method to obtain the constructed product, So concrete builder can implement
+ *  Concrete Builder - Builder method definition
+ *  Director - It controls the product construction process, know the sequence to use the product (the manual) and a method to obtain the constructed product
+ * 
+ * 
+ *  Product: Pizza
+ *      - Represents an individual pizza with its selected toppings, crust type, and size.
+ *  
+ * Builder Interface: PizzaBuilder
+ *      - Defines methods for constructing a pizza.
+ *      - Allows setting different toppings, crust type, and size.
+ *      - Provides a method to obtain the constructed pizza.
+ *  
+ * Concrete Builder: CustomPizzaBuilder
+ *      - Implements the PizzaBuilder interface.
+ *      - Provides methods for adding toppings, selecting crust type and size.
+ *      - Maintains the internal state of the pizza being built.
+ *  
+ * Director: PizzaOrderDirector
+ *      - Controls the pizza construction process using the PizzaBuilder.
+ *      - Defines the steps and order in which toppings, crust type, and size are set.
+ *      - Provides a method to obtain the final constructed pizza.
+ * 
+ * 
+ */
+
