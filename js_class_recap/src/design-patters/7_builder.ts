6ed46aacd4d2c3a6233cf8c6f4e6700110b3071d
Sankar Prasad Biswas <sankarbiswas07@gmail.com>
2023-07-13 23:20:54 +0530

    pattern - 7 - builder - question form chatGPT + thought process

---
+++ js_class_recap/src/design-patters/7_builder.ts
@@ -0,0 +1,8 @@
+/**
+ * 
+ * Problem Domain: Imagine you are developing a pizza ordering system where 
+ * customers can customize their pizzas with various toppings, crust types, and sizes. 
+ * Each pizza can have different combinations of toppings, 
+ * and the order can include multiple pizzas with different specifications.
+ * 
+ */
